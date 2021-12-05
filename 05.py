547a0e56d36c1a40de1771585d925ed3eb0d8699
Albert ten Oever <agtoever@hotmail.com>
2021-12-05 22:51:24 +0100

    Improved algorithm

--- 05.py
+++ 05.py
@@ -4,6 +4,7 @@ See: https://adventofcode.com/2021/day/5
 """
 
 import numpy
+import collections
 import logging
 logging.basicConfig(level=logging.INFO)
 
@@ -58,17 +59,44 @@ def lines_to_graph(lines: numpy.array,
     return graph
 
 
+def lines_to_pointdict(lines: numpy.array,
+                       only_straight: bool = True) -> dict:
+    points = collections.defaultdict(int)
+
+    for from_xy, to_xy in lines:
+        assert ((from_xy[0] == to_xy[0]) or  # vertical line
+                (from_xy[1] == to_xy[1]) or  # horizontal line
+                (abs(to_xy[0] - from_xy[0]) == abs(to_xy[1] - from_xy[1])))
+                # diagnonal line
+
+        dx = 1 if from_xy[0] <= to_xy[0] else -1
+        dy = 1 if from_xy[1] <= to_xy[1] else -1
+
+        if _is_straight(from_xy, to_xy) or not only_straight:
+            x_range = list(range(from_xy[0], to_xy[0] + dx, dx))
+            y_range = list(range(from_xy[1], to_xy[1] + dy, dy))
+            if len(x_range) == 1:
+                x_range = x_range * len(y_range)
+            elif len(y_range) == 1:
+                y_range = y_range * len(x_range)
+
+            for x, y in zip(x_range, y_range):
+                points[tuple([x, y])] += 1
+
+    return points
+
+
 def main():
     lines = read_data()
 
-    graph = lines_to_graph(lines, only_straight=True)
-    num_overlap = len(graph[graph >= 2])
+    points = lines_to_pointdict(lines, True)
+    num_overlap = sum(1 for ct in points.values() if ct >= 2)
 
     print('*** First part of the assignment ***')
     print(f'Number of overlaps >= 2 is: {num_overlap}')
 
-    graph = lines_to_graph(lines, only_straight=False)
-    num_overlap = len(graph[graph >= 2])
+    points = lines_to_pointdict(lines, False)
+    num_overlap = sum(1 for ct in points.values() if ct >= 2)
 
     print('\n*** Second part of the assignment ***')
     print(f'Number of overlaps >= 2 is: {num_overlap}')
