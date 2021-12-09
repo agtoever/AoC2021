263cbd420bf3fbeeddd3c5a163d2dbd5baa63499
Albert ten Oever <agtoever@hotmail.com>
2021-12-09 17:22:51 +0100

    Cleaned up code of day 9

--- 09.py
+++ 09.py
@@ -1,14 +1,76 @@
+#!python3
 """
 Day 9 of Advent of Code 2021
 See: https://adventofcode.com/2021/day/9
 """
 
+from pathlib import Path
 import numpy as np
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 
 IMPORT_FILE = '09.input'
+
+
+def import_heights(filename: str) -> np.array:
+    """Import file from filename into a numpy array
+
+    The file is assumed to contain values from 0-9 without separators.
+
+    Args:
+        filename (str): name of the file to be read
+
+    Returns:
+        np.array: Numpy array with the data from the file
+    """
+    # Get the path of this Python file
+    path = Path(__file__).with_name(filename)
+
+    # Open file in the same path as this Python file
+    with path.open('rt') as f:
+        return np.array([[int(c) for c in list(s.strip())] for s in f])
+
+
+def sum_lows(heights: np.array) -> int:
+    """Find all low points in the heights array and add their values + 1
+
+    Low points are point surrounded in horizontal and vertical directions
+    with only higher values than themselves. Each low point is increased
+    with 1 and then added together.
+
+    Args:
+        heights (np.array): 2d array with values
+
+    Returns:
+        int: sum of the low points
+
+    Example:
+    #  Low points are: 1, 0, 5 and 5. Each increased by 1 and summed is 15
+    >>> sum_lows(np.array([ \
+            [2, 1, 9, 9, 9, 4, 3, 2, 1, 0], \
+            [3, 9, 8, 7, 8, 9, 4, 9, 2, 1], \
+            [9, 8, 5, 6, 7, 8, 9, 8, 9, 2], \
+            [8, 7, 6, 7, 8, 9, 6, 7, 8, 9], \
+            [9, 8, 9, 9, 9, 6, 5, 6, 7, 8]]))
+    15
+    """
+    logging.debug(f'Finding low points for {heights}')
+    # Pad heights with 10's
+    padded_heights = np.pad(heights, pad_width=1,
+                            mode='constant', constant_values=10)
+
+    # Min heights = True if cell is lower than all of the adjacent cells
+    min_heights = np.array(
+        [[all(padded_heights[r, c] < padded_heights[r + dr, c + dc]
+              for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)])
+          for c in range(1, heights.shape[1] + 1)]
+         for r in range(1, heights.shape[0] + 1)])
+
+    logging.debug(f'Boolean array with True at Low points: {min_heights=}')
+
+    # Sum the points where heigt == min_height + 1
+    return sum(heights[min_heights] + 1)
 
 
 def fill_count(heights: np.array) -> np.array:
@@ -19,10 +81,27 @@ def fill_count(heights: np.array) -> np.array:
 
     Returns:
         np.array: 2d numpy area with 0 as border and 1, 2, 3... as area's
-    
+
     Example:
-    >>> import numpy as np
-    >>> 
+    >>> areas = fill_count(np.array([ \
+            [2, 1, 9, 9, 9, 4, 3, 2, 1, 0], \
+            [3, 9, 8, 7, 8, 9, 4, 9, 2, 1], \
+            [9, 8, 5, 6, 7, 8, 9, 8, 9, 2], \
+            [8, 7, 6, 7, 8, 9, 6, 7, 8, 9], \
+            [9, 8, 9, 9, 9, 6, 5, 6, 7, 8]]))
+
+    >>> areas
+    array([[1, 1, 0, 0, 0, 2, 2, 2, 2, 2],
+           [1, 0, 3, 3, 3, 0, 2, 0, 2, 2],
+           [0, 3, 3, 3, 3, 3, 0, 4, 0, 2],
+           [3, 3, 3, 3, 3, 0, 4, 4, 4, 0],
+           [0, 3, 0, 0, 0, 4, 4, 4, 4, 4]])
+
+    # Now you can easily calculate the sizes of the area's.
+    # Note that area 0 is the border.
+    >>> dict(zip(*np.unique(areas, return_counts=True)))
+    {0: 15, 1: 3, 2: 9, 3: 14, 4: 9}
+
     """
     # Create a copy of heights. Set border to -1 and fill area's to -2
     h = np.copy(heights)
@@ -38,6 +117,9 @@ def fill_count(heights: np.array) -> np.array:
         indices = [list(zip(*np.where(h == -2)))[0]]
         h[indices[0]] = area_counter
 
+        logging.debug(f'Filling area {area_counter}, '
+                      f'starting at index {indices[0]}')
+
         # Keep going until the area can't be expanded anymore
         while area_size > old_area_size:
             old_area_size = area_size
@@ -52,6 +134,8 @@ def fill_count(heights: np.array) -> np.array:
                         indices.append(tuple([r + dr, c + dc]))
                         area_size += 1
 
+        logging.debug(f'Filled area {area_counter} of size {area_size}')
+
         # Go to the next area
         area_counter += 1
 
@@ -62,30 +146,20 @@ def fill_count(heights: np.array) -> np.array:
 
 
 def main():
-    # Import data into numpy array
-    with open(IMPORT_FILE, 'rt') as f:
-        heights = np.array([[int(c) for c in list(s.strip())] for s in f])
-
-    # Pad heights with 10's
-    padded_heights = np.pad(heights, pad_width=1,
-                            mode='constant', constant_values=10)
-
-    # Min heights = True if cell is lower than all of the adjacent cells
-    min_heights = np.array(
-        [[all(padded_heights[r, c] < padded_heights[r + dr, c + dc]
-              for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)])
-          for c in range(1, heights.shape[1] + 1)]
-         for r in range(1, heights.shape[0] + 1)])
-
-    # Sum the points where heigt == min_height + 1
-    sum_lows = sum(heights[min_heights] + 1)
+    heights = import_heights(IMPORT_FILE)
+    low_sum = sum_lows(heights)
 
     print('*** First part of the assignment ***')
-    print(f'Sum of lows is {sum_lows}')
+    print(f'Sum of lows is {low_sum}')
 
+    # Mark all area's, count their sizes
     marked = fill_count(heights)
     area_counts = dict(zip(*np.unique(marked, return_counts=True)))
     del area_counts[0]  # remove the border itself from the counts
+
+    logging.debug(f'Found areas (index: size): {area_counts}')
+
+    # Multiply the largest 3 sizes for the answer
     product_largest_3 = np.prod(sorted(area_counts.values())[-3:])
 
     print('\n*** Second part of the assignment ***')
@@ -93,4 +167,6 @@ def main():
 
 
 if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
     main()
